6392380dc5dfd598a588d3affe61a8683c15a29e
Collin Haws <117249176+CHawsCoding@users.noreply.github.com>
2024-02-20 12:03:10 -0700

    Added a landing.ejs as the homepage for our website. - fixed our server.js file to include the route to open our views folder and added our rendering engine (ejs)

--- server.js
+++ server.js
@@ -9,25 +9,38 @@ require('dotenv').config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.set('view engine', 'ejs');
+
+// Set the directory where the view templates are located
+app.set('views', 'src/views');
+
+
+app.use(express.static('public'));
+
 mongoose.connect('mongodb://localhost/yourDatabaseName', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 })
-.then(() => console.log('Connected to MongoDB...'))
-.catch(err => console.error('Could not connect to MongoDB...', err));
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.error('Could not connect to MongoDB...', err));
 
 app.use(cors());
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
-  });
-  
+});
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    res.render('pages/landing', {
+        title: 'Welcome'
+    });
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+    console.log(`Server is running on port ${PORT}. Open http://localhost:${PORT} in your browser.`);
+  });
